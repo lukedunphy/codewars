91f9a3ea6697aec657f69702b8c685e7adb8d2b4
lukedunphy <ltdunphy@asu.edu>
2024-11-06 15:01:28 -0500

    completed codewars assignment

---
+++ first_codewars.py
@@ -0,0 +1,25 @@
+# (1) - Even or Odd
+# def even_or_odd(number):
+#     pass
+#     if number % 2 == 0:
+#         return 'Even'
+#     else:
+#         return 'Odd'
+    
+# # (2) - Convert a Number to a String
+# def number_to_string(num):
+#     # Return a string of the number here!
+#     num = str(num)
+#     return num
+
+# (3) - Vowel Count
+def get_count(sentence):
+    pass
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for char in sentence:
+        if char in vowels:
+            count += 1
+    return count
+print(get_count('whats going on little buddy'))
+    
